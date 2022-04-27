4c18057a6e2e5329b9d5a246ad314e4fec16bfd7
Manas <manaslegodesigns@gmail.com>
2022-04-28 00:26:52 +0530

    change tokens

--- bg_bot.py
+++ bg_bot.py
@@ -36,19 +36,19 @@ def runserver():
             if phone_no not in users.keys() and ( msg_text.lower().strip() == 'hare krishna' or msg_text.lower().strip() == 'hare krisna' or msg_text.lower().strip() == 'hare krsna'):
                 users[phone_no] = [1, True, name]
                 encoded_msg = urllib.parse.quote('*Hare Krishna {}!* \n\nYou are now subscribed to receive daily Bhagvad Gita shlokas. \n\nYou will receive a message every day at 5:00 AM. \n\nYou can unsubscribe anytime by sending "unsubscribe" to this number. \n\nYour journey of self realisation starts now.'.format(name))
-                return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=6268CD836C83B&access_token=dfcd47b5105a80e08c6d5e7d8d2bfa60'.format(phone_no, encoded_msg)
+                return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=62699138DB07C&access_token=cc6f1a6809d274606d371de2c95b809a'.format(phone_no, encoded_msg)
                 urllib.request.urlopen(return_webhook_url)
 
             elif phone_no in users.keys() and ( msg_text.lower().strip() == 'hare krishna' or msg_text.lower().strip() == 'hare krisna' or msg_text.lower().strip() == 'hare krsna'):
                 users[phone_no] = [users[phone_no][0], True, name]
                 encoded_msg = urllib.parse.quote('*Hare Krishna {}!* \n\nYou are now subscribed to receive daily Bhagvad Gita shlokas. \n\nYou will receive a message every day at 5:00 AM. \n\nYou can unsubscribe anytime by sending "unsubscribe" to this number. \n\nYour journey of self realisation starts now.'.format(name))
-                return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=6268CD836C83B&access_token=dfcd47b5105a80e08c6d5e7d8d2bfa60'.format(phone_no, encoded_msg)
+                return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=62699138DB07C&access_token=cc6f1a6809d274606d371de2c95b809a'.format(phone_no, encoded_msg)
                 urllib.request.urlopen(return_webhook_url)
 
             elif phone_no in users.keys() and msg_text.lower().strip() == 'unsubscribe':
                 users[phone_no][1] = False
                 encoded_msg = urllib.parse.quote('You have been unsubscribed from Bhagavad Gita notifications. \n\nYou can resubscribe anytime by sending "hare krishna" to this number.')
-                return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=6268CD836C83B&access_token=dfcd47b5105a80e08c6d5e7d8d2bfa60'.format(phone_no, encoded_msg)
+                return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=62699138DB07C&access_token=cc6f1a6809d274606d371de2c95b809a'.format(phone_no, encoded_msg)
                 urllib.request.urlopen(return_webhook_url)
 
     except:
@@ -96,7 +96,7 @@ def print_date_time():
                 message_text = result['slok'] + '\n\n' + result['transliteration'] + '\n\nCommentary by ' + result['siva']['author'] + '\n\nTranslation: ' + result['siva']['et'] + '\n\nWord By Word Meaning:' + wrd_by_wrd_translation + '\n\nCommentary: ' + commentary
 
             encoded_msg = urllib.parse.quote(message_text)
-            return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=6268CD836C83B&access_token=dfcd47b5105a80e08c6d5e7d8d2bfa60'.format(phone_no, encoded_msg)
+            return_webhook_url = 'https://betablaster.in/api/send.php?number={}&type=text&message={}&instance_id=62699138DB07C&access_token=cc6f1a6809d274606d371de2c95b809a'.format(phone_no, encoded_msg)
             print(return_webhook_url)
             urllib.request.urlopen(return_webhook_url)
             users[phone_no][0] = users[phone_no][0] + 1
